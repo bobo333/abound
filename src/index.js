dccad4116b5274ed06b6a1d25651e91b70c0b476
Steven Cipriano <redsox333@gmail.com>
2019-09-19 22:07:57 -0700

    variable interest rates

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ class Calculator extends React.Component {
       monthlyIncome: '',
       monthlySpend: '',
       totalSavings: '',
-      currentSlide: 0
+      currentSlide: 0,
+      rates: {
+        withdrawal: 0.04,
+        inflation: 0.035,
+        growth: 0.075,
+        income: 0.05,
+      }
     };
   }
 
@@ -29,9 +35,45 @@ class Calculator extends React.Component {
     this.setState({totalSavings: event.target.value});
   }
 
+  updateInflation(event) {
+    var newRates = this.state.rates;
+    newRates.inflation = event.target.value;
+    this.setState({rates: newRates});
+  }
+
+  updateWithdrawal(event) {
+    var newRates = this.state.rates;
+    newRates.withdrawal = event.target.value;
+    this.setState({rates: newRates});
+  }
+
+  updateGrowth(event) {
+    var newRates = this.state.rates;
+    newRates.growth = event.target.value;
+    this.setState({rates: newRates});
+  }
+
+  updateIncomeRate(event) {
+    var newRates = this.state.rates;
+    newRates.income = event.target.value;
+    this.setState({rates: newRates});
+  }
+
   nextSlide(event) {
     event.preventDefault();
     this.setState({currentSlide: this.state.currentSlide + 1});
+  }
+
+  sanitizeRates(rates) {
+    const newRates = {}
+    for (var key in rates) {
+      newRates[key] = this.sanitizeInput(rates[key])
+    }
+    return newRates
+  }
+
+  sanitizeInput(input) {
+    return parseFloat(input) || 0;
   }
 
   previousSlide(event) {
@@ -58,7 +100,11 @@ class Calculator extends React.Component {
                    backOnClick={(e) => this.previousSlide(e)} />
       );
     } else {
-      const graphData = calculateGraphData(parseFloat(this.state.monthlySpend), parseFloat(this.state.monthlyIncome), parseFloat(this.state.totalSavings));
+      const graphData = calculateGraphData(
+        this.sanitizeInput(this.state.monthlySpend),
+        this.sanitizeInput(this.state.monthlyIncome),
+        this.sanitizeInput(this.state.totalSavings),
+        this.sanitizeRates(this.state.rates));
 
       return (
         <div>
@@ -71,6 +117,18 @@ class Calculator extends React.Component {
           <div>savings:
             <input type='text' placeholder='$250000' value={this.state.totalSavings} onChange={(e) => this.updateSavings(e)} />
           </div>
+          <div>inflation (%):
+            <input type='text' placeholder='0.035' value={this.state.rates.inflation} onChange={(e) => this.updateInflation(e)} />
+          </div>
+          <div>withdrawal rate (%):
+            <input type='text' placeholder='0.04' value={this.state.rates.withdrawal} onChange={(e) => this.updateWithdrawal(e)} />
+          </div>
+          <div>growth rate (%):
+            <input type='text' placeholder='0.075' value={this.state.rates.growth} onChange={(e) => this.updateGrowth(e)} />
+          </div>
+          <div>income increase rate (%):
+            <input type='text' placeholder='0.05' value={this.state.rates.income} onChange={(e) => this.updateIncomeRate(e)} />
+          </div>
           <input type='button' value='Back' onClick={(e) => this.previousSlide(e)} />
           <Chart data={graphData} />
           <div><pre>{JSON.stringify(graphData, null, 2)}</pre></div>
@@ -79,8 +137,6 @@ class Calculator extends React.Component {
     }
   }
 }
-
-// ========================================
 
 ReactDOM.render(
   <Calculator />,
